c351ee464a45ab2a35c3430569b7ae64df0d453a
Naeem Uddin <85982494+nu2911@users.noreply.github.com>
2024-11-11 00:28:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+var noOfDrum = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < noOfDrum; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var btnHtml = this.innerHTML;
+    makeSound(btnHtml);
+    btnAnime(btnHtml);
+
+    document.addEventListener("keydown", function (event) {
+      makeSound(event.key);
+      btnAnime(event.key);
+    });
+
+    function makeSound(key) {
+      switch (key) {
+        case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+
+        case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+
+        case "l":
+          var kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+
+        default:
+          console.log(btnHtml);
+          break;
+      }
+    }
+    function btnAnime(currentKey) {
+      var activeBtn = document.querySelector("." + currentKey);
+      activeBtn.classList.add("pressed");
+
+      setTimeout(function() {
+        activeBtn.classList.remove("pressed");
+      },100)
+    }
+
+  });
+}
